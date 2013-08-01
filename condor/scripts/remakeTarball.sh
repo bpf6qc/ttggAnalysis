78bf32a5789c4a5e06a6d3d45a2f7bd6b7b71789
bpf6qc <bpf6qc@virginia.edu>
2013-08-01 00:19:12 -0400

    script permissions

--- condor/scripts/remakeTarball.sh
+++ condor/scripts/remakeTarball.sh
@@ -3,7 +3,7 @@
 WORK_DIR=`pwd`
 
 cd ../../../../
-tar -czf src.tgz `cat $WORK_DIR/../doc/tarballFileList.txt`
+tar -czf src.tgz `cat $WORK_DIR/doc/tarballFileList.txt`
 mv src.tgz $WORK_DIR/../
 
 cd $WORK_DIR
